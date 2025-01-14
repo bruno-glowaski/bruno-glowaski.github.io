93364d484eb8526cf4579fb8c740be762cf47b2f
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2025-01-14 16:33:35 -0400

    misc: add bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import withBundlerAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  experimental: { webpackMemoryOptimizations: true },
 };
 
-export default nextConfig;
+export default withBundlerAnalyzer({
+  enabled: process.env.ANALYZE == "true",
+  openAnalyzer: true,
+})(nextConfig);
