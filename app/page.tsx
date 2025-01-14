698ad6875f548bce728cf60d8cbfe7e837ad82d7
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2025-01-14 16:33:27 -0400

    feat: homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,90 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div>
+      <nav>
+        <a href="/">Home</a>
+        <a
+          href="https://github.com/bruno-glowaski"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Projects
+        </a>
+        <a href="/resume" target="_blank">
+          Resumé
+        </a>
+        <a
+          href="https://www.linkedin.com/in/bruno-glowaski"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About
+        </a>
+      </nav>
+      <header>
+        <h1>Bruno Glowaski</h1>
+        <span>Software Engineer</span>
+      </header>
+      <main>
+        <h2>About</h2>
+        <p>
+          Hello, my name is <em>Bruno Henrique Glowaski Morais</em> and I’m a
+          passionate
+          <strong>software engineer</strong>. Being a curious person, I dabble
+          in a lot of fields, including, but not limited to,{" "}
+          <em>
+            computer graphics, operating systems, security, software development
+            processes & software quality
+          </em>
+          . In my professional time, I can work with{" "}
+          <strong>
+            full-stack services, game frameworks and/or engines, low-level
+            system components and mobile and/or desktop apps
+          </strong>
+          . During my professional career, I’ve worked in multiple kinds of
+          teams, from basically alone to somewhat large multidisciplinary teams.
+        </p>
+        <a
+          href="https://www.linkedin.com/in/bruno-glowaski"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <section>
+        <h2>Buy me chocolate milk</h2>
+        <ul>
+          <li>
+            <h3>Bitcoin</h3>
+            <span>
+              bc1qjgphzkvgmlr909x4xg06w0jq2drzj9ttcgr53laz4s8ykduxvp0qtf0kn3
+            </span>
+          </li>
+          <li>
+            <h3>PIX</h3>
+            <span>9edee982-92e4-49bb-9927-643e68643d45</span>
+          </li>
+        </ul>
+      </section>
+      <footer>
+        <nav>
+          <a
+            href="https://github.com/bruno-glowaski"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bruno-glowaski"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </nav>
+        <span>Made with Next.js, Tailwind CSS and TypeScript</span>
       </footer>
     </div>
   );
